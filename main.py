decc645b36f6999ab18f5f32be798f4b66da532c
Bhankumari <132442597+Bhankumari@users.noreply.github.com>
2025-06-25 07:49:04 +0545

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ class BotTelegramSender:
 
 class PortfolioMACDAnalyzer:
     def __init__(self, tg_bot_token=None, tg_group_chat_ids=None, tg_personal_chat_ids=None):
-        self.my_stocks = ['TVCL', 'RURU', 'HBL', 'ICFC', 'JBLB', 'JFL', 'BHDC','TVCL','MHNL']
+        self.my_stocks = ['TVCL', 'RURU', 'HBL', 'ICFC', 'JBLB', 'JFL', 'BHDC','TVCL','MHNL','JOSHI']
         self.portfolio_data = []
         self.macd_signals = {}
         self.rsi_alerts = {}
